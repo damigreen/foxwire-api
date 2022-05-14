79c25ba4f1209115054a57feb0e5f9873636f633
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-14 21:01:50 +0100

    Role model

--- app/Models/Role.ts
+++ app/Models/Role.ts
@@ -1,13 +1,18 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Role extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
-  @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  @column()
+  public name: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  @column()
+  public code: string
+
+  @column()
+  public description: string
+
+  @column()
+  public active: boolean
 }
