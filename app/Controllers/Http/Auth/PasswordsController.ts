05fd6c61d577b3581de3506469ab43c0a06d6a76
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-19 11:29:58 +0100

    Password controller

--- app/Controllers/Http/Auth/PasswordsController.ts
+++ app/Controllers/Http/Auth/PasswordsController.ts
@@ -1,7 +1,45 @@
+import Event from '@ioc:Adonis/Core/Event';
+import User from 'App/Models/User';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import ResetCodeValidator from 'App/Validators/Password/ResetCodeValidator'
+import PasswordReset from 'App/Models/Auth/PasswordReset';
 
 export default class PasswordsController {
     async sendCode({ request, response }: HttpContextContract) {
-        return response.json("reset password")
+        const payload = await request.validate(ResetCodeValidator);
+
+        const {
+            email, phone, type
+        } = payload
+
+        const user = await User.query()
+            .where("email", email ? email : "")
+            .orWhere("phone", phone ? phone : "")
+            .firstOrFail()
+
+        let resetCode
+        do {
+            resetCode = Math.floor(Math.random() * 90000) + 10000;
+        } while (await PasswordReset.findBy("code", resetCode) != null)
+
+        const oldPasswordResetCode = await PasswordReset.query().where("user_id", user.id)
+
+        for (let i = 0; i < oldPasswordResetCode.length; i++) {
+            oldPasswordResetCode[i]?.delete()
+        }
+
+        PasswordReset.create({
+            userId: user.id,
+            code: resetCode,
+            used: false
+        })
+
+        await Event.emit("password/reset-code-generated", { user, resetCode, type })
+        await Event.emit("user/created", { user })
+        
+        return response.json({
+            status: true,
+            message: `a password reset code has been sent to your ${type}`
+        })
     }
 }
