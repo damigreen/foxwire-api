5f8c95a0e09a9d15894affce3faa848eebfc422a
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-22 18:05:35 +0100

    Ctx extended class

--- contracts/ctxExtend.ts
+++ contracts/ctxExtend.ts
@@ -1,10 +1,18 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
-import { TypedSchema, ParsedTypedSchema, Schema, Rules } from "@ioc:Adonis/Core/Validator"
+import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import {
+    TypedSchema,
+    ParsedTypedSchema,
+    Schema,
+    Rules,
+} from "@ioc:Adonis/Core/Validator";
 
 export default interface CtxExtendContract extends HttpContextContract {
     validator: {
         rules: Rules;
         schema: Schema;
-        validate<T extends TypedSchema>(schema: T): Promise<ParsedTypedSchema<T>["props"]>
-    }
+        validate<T extends TypedSchema>(
+            schema: T
+        ): Promise<ParsedTypedSchema<T>["props"]>;
+        validate<T extends ParsedTypedSchema<TypedSchema>>(schema: T): Promise<T['props']>;
+    };
 }
