d615a1c6389c08a9be7f12b3068fc39b46a336af
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-23 14:04:42 +0100

    Role user

--- app/Models/People/Role.ts
+++ app/Models/People/Role.ts
@@ -1,5 +1,4 @@
-import User from 'App/Models/User';
-import { BaseModel, column, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Role extends BaseModel {
   @column({ isPrimary: true })
---
+++ database/migrations/1652475982534_role_user.ts
@@ -0,0 +1,28 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class RoleUsers extends BaseSchema {
+  protected tableName = 'role_user'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table
+        .integer("role_id")
+        .unsigned()
+        .references("id")
+        .inTable("roles")
+        .onDelete("CASCADE")
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
