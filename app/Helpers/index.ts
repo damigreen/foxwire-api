a0d7a1571596c0394aa8531a4360c8eb393b3e9b
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-25 18:14:27 +0100

    Helper functions

--- app/Helpers/index.ts
+++ app/Helpers/index.ts
@@ -2,7 +2,10 @@ import User from 'App/Models/User';
 
 
 export const getRoleCodes = (user?: User) => {
-    console.log(user);
     return user?.toJSON().roles.map(role => role.code) || [];
+}
+
+export const hashCode = function (s) {
+    return s.split("").reduce(function (a, b) { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0);
 }
 
