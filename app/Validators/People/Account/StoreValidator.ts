6c9bde997eb4529b2ff5619e5b9c2f1d14f283b2
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-25 18:14:02 +0100

    Account request validators

---
+++ app/Validators/People/Account/StoreValidator.ts
@@ -0,0 +1,59 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+const schemaToCreate = {
+  name: schema.string({}, [rules.unique({ table: 'accounts', column: 'name' })]),
+  userId: schema.number(),
+  accountTypeId: schema.number(),
+}
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) { }
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create(schemaToCreate)
+
+  public static parsedSchema = schema.create(schemaToCreate)
+
+  /**
+ * The `schema` first gets compiled to a reusable function and then that compiled
+ * function validates the data at runtime.
+ *
+ * Since, compiling the schema is an expensive operation, you must always cache it by
+ * defining a unique cache key. The simplest way is to use the current request route
+ * key, which is a combination of the route pattern and HTTP method.
+ */
+  public cacheKey = this.ctx.routeKey;
+
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {}
+}
