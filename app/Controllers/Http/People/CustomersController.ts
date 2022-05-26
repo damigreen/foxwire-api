22eb1fce8aadd5808bf27dec5b2177df98475898
Damilola Faseun <damilola.faseun@gmail.com>
2022-05-26 11:49:30 +0100

    Customer logic

--- app/Controllers/Http/People/CustomersController.ts
+++ app/Controllers/Http/People/CustomersController.ts
@@ -1,20 +1,78 @@
+import { schema } from '@ioc:Adonis/Core/Validator';
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Customer from 'App/Models/People/Customer';
+import Create from 'App/Repos/People/Customer/Create';
+import Update from 'App/Repos/People/Customer/Update';
 
 
 export default class CustomersController {
-  public async index({}: HttpContextContract) {}
+  public async index({ request, response }: HttpContextContract) {
+    var {
+      associations = [],
+      page = 1,
+      perPage = 100,
+      sortBy = "created_at",
+      sortOrder = "asc"
+    } = request.get();
 
-  public async create({}: HttpContextContract) {}
+    var query = Customer.query().orderBy(sortBy, sortOrder)
 
-  public async store({}: HttpContextContract) {
-    // const payload = request.validate
+    for (const association of associations) query.preload(association);
+
+    const results = await query.paginate(page, perPage)
+
+    return response.json(results)
   }
 
-  public async show({}: HttpContextContract) {}
+  // public async create({ }: HttpContextContract) { }
 
-  public async edit({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const payload = await request.validate({
+      schema: schema.create({
+        userId: schema.number()
+      })
+    })
 
-  public async update({}: HttpContextContract) {}
+    const customer = await new Create().handle(payload)
 
-  public async destroy({}: HttpContextContract) {}
+    return response.json({
+      status: true,
+      customer
+    })
+  }
+
+  public async show({ params, request, response }: HttpContextContract) {
+    const { associations = [] } = request.get()
+
+    let query = Customer.query()
+      .where("id", params.id)
+
+    for (const association of associations) query = query.preload(association)
+
+    const customer = await query.firstOrFail()
+
+    return response.json({
+      status: true,
+      customer,
+    })
+  }
+
+  // public async edit({ }: HttpContextContract) { }
+
+  public async update({ params, request, response }: HttpContextContract) {
+    const payload = await request.validate({
+      schema: schema.create({
+        userId: schema.number()
+      })
+    })
+
+    const customer = await new Update().handle({ id: params.id, ...payload })
+
+    return response.json({
+      status: true,
+      customer
+    })
+  }
+
+  // public async destroy({ }: HttpContextContract) { }
 }
