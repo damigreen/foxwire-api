1f4a12e500a0bd4f34d02f8e955091865f0be6c6
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-06 19:29:26 +0100

    Database seeder

---
+++ database/seeders/Database.ts
@@ -0,0 +1,14 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import RoleSeeder from './store/Role'
+import AccountTypeSeeeder from './store/AccountType';
+import UserSeeder from './store/User';
+
+
+export default class DatabaseSeeder extends BaseSeeder {
+  public async run() {
+    await RoleSeeder.run();
+    await AccountTypeSeeeder.run();
+    await AccountTypeSeeeder.run();
+    await UserSeeder.run();
+  }
+}
