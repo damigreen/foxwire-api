22865ed4e5554ef24bfb6fecaf5190f7a4ca02d9
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-12 20:11:38 +0100

    Event trigger schema

--- app/Models/System/SystemEventTrigger.ts
+++ app/Models/System/SystemEventTrigger.ts
@@ -1,26 +1,43 @@
-import { BaseModel, column, hasMany, HasMany, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
-import SystemEntity from 'App/Models/System/SystemEntity';
-import SystemEvent from 'App/Models/System/SystemEvent';
+import {
+  scope,
+  BaseModel,
+  column,
+  hasMany,
+  HasMany,
+  belongsTo,
+  BelongsTo,
+} from "@ioc:Adonis/Lucid/Orm";
+import SystemEntity from "App/Models/System/SystemEntity";
+import SystemEvent from "App/Models/System/SystemEvent";
+import User from "App/Models/User";
+import { getRoleCodes } from "App/Helpers";
 
 export default class SystemEventTrigger extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public name: string
+  public name: string;
 
   @column()
-  public code: string
+  public code: string;
 
   @column()
-  public description: string
+  public description: string;
 
   @column()
-  public SystemEntityId: number
+  public SystemEntityId: number;
 
   @hasMany(() => SystemEvent)
-  public events: HasMany<typeof SystemEvent>
+  public events: HasMany<typeof SystemEvent>;
 
   @belongsTo(() => SystemEntity)
-  public entity: BelongsTo<typeof SystemEntity>
+  public entity: BelongsTo<typeof SystemEntity>;
+
+  // Scopes
+  public static byUser = scope((query, user?: User) => {
+    const roleCodes = getRoleCodes(user);
+    if (roleCodes.find((code) => code == "administrator")) return query;
+    return query.where("id", user?.id || 0);
+  });
 }
