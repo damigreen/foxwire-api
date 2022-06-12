7a0de4d5f83d87f03cba89165e069f9215346dbd
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-12 20:11:14 +0100

    Web hook params logic

---
+++ app/Controllers/Http/System/WebHookParamsController.ts
@@ -0,0 +1,79 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import WebHookVerb from "App/Models/System/WebHookVerb";
+
+/**
+ * Resourceful controller for interacting with webhookparams
+ */
+export default class WebHookParamsController {
+  /**
+   * Show a list of all webhookparams.
+   * GET webhookparams
+   *
+   * @param {object} ctx
+   * @param {Auth} ctx.auth
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  public async index({ auth, request, response }: HttpContextContract) {
+    var {
+      associations = [],
+      page = 1,
+      perPage = 100,
+      sortBy = "name",
+      sortOrder = "asc",
+    } = request.get();
+
+    var query = WebHookVerb.query().orderBy(sortBy, sortOrder);
+
+    for (const association of associations) query.preload(association);
+
+    query.apply((scopes) => scopes.byUser(auth.user));
+
+    const results = await query.paginate(page, perPage);
+
+    return response.json(results);
+  }
+
+  /**
+   * Display a single webhookparam.
+   * GET webhookparams/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {Params} ctx.params
+   */
+  public async show({ params, request, response }: HttpContextContract) {
+    const { associations = [] } = request.get();
+
+    let query = WebHookVerb.query().where("id", params.id);
+
+    for (const association of associations) query = query.preload(association);
+
+    const user = await query.firstOrFail();
+
+    return response.json({
+      status: true,
+      user,
+    });
+  }
+  
+  /**
+   * Delete a webhookparam with id.
+   * DELETE webhookparams/:id
+   *
+   * @param {object} ctx
+   * @param {Params} ctx.params
+   * @param {Response} ctx.response
+   */
+  public async destroy({ params, response }: HttpContextContract) {
+    let webHookVerb = WebHookVerb.query().where("id", params.id);
+
+    await webHookVerb.delete();
+
+    return response.json({
+      status: true,
+      message: "Web hook header deleted",
+    });
+  }
+}
