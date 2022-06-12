b14433fbd06a87f8d65b97c794684b213a2446a0
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-12 18:29:01 +0100

    Web hook header schema

--- app/Models/System/WebHookHeader.ts
+++ app/Models/System/WebHookHeader.ts
@@ -1,29 +1,44 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
-import WebHook from 'App/Models/System/WebHook';
+import { DateTime } from "luxon";
+import {
+  scope,
+  BaseModel,
+  column,
+  manyToMany,
+  ManyToMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import User from "App/Models/User";
+import WebHook from "App/Models/System/WebHook";
+import { getRoleCodes } from "App/Helpers";
 
 export default class WebHookHeader extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public name: string
+  public name: string;
 
   @column()
-  public value: string
+  public value: string;
 
   @column()
-  public accountId: number
+  public accountId: number;
 
   @column.dateTime({ autoCreate: true })
-  public createdAt: DateTime
+  public createdAt: DateTime;
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  public updatedAt: DateTime
+  public updatedAt: DateTime;
 
   @column.dateTime()
-  public deletedAt: DateTime
+  public deletedAt: DateTime;
 
   @manyToMany(() => WebHook)
-  public webHooks: ManyToMany<typeof WebHook>
+  public webHooks: ManyToMany<typeof WebHook>;
+  
+  // Scopes
+  public static byUser = scope((query, user?: User) => {
+    const roleCodes = getRoleCodes(user);
+    if (roleCodes.find((code) => code == "administrator")) return query;
+    return query.where("id", user?.id || 0);
+  });
 }
