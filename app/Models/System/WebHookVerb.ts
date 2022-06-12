2c6e70ee594da59df490b3044d7321d5d3fd55b3
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-12 18:52:51 +0100

    Webhook verb schema

--- app/Models/System/WebHookVerb.ts
+++ app/Models/System/WebHookVerb.ts
@@ -1,17 +1,32 @@
-import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
-import WebHook from 'App/Models/System/WebHook'
+import {
+  scope,
+  BaseModel,
+  column,
+  hasMany,
+  HasMany,
+} from "@ioc:Adonis/Lucid/Orm";
+import User from "App/Models/User";
+import WebHook from "App/Models/System/WebHook";
+import { getRoleCodes } from "App/Helpers";
 
 export default class WebHookVerb extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: number;
 
   @column()
-  public name: string
+  public name: string;
 
   @column()
-  public code: string
+  public code: string;
 
   // Relationships
   @hasMany(() => WebHook)
-  public webHooks: HasMany<typeof WebHook>
+  public webHooks: HasMany<typeof WebHook>;
+
+  // Scopes
+  public static byUser = scope((query, user?: User) => {
+    const roleCodes = getRoleCodes(user);
+    if (roleCodes.find((code) => code == "administrator")) return query;
+    return query.where("id", user?.id || 0);
+  });
 }
