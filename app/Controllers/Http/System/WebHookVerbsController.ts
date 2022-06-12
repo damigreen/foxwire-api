a1fc518f113a5f1f43a8263a519ac5951c9eabbd
Damilola Faseun <damilola.faseun@gmail.com>
2022-06-12 18:52:30 +0100

    Webhook verb logic

---
+++ app/Controllers/Http/System/WebHookVerbsController.ts
@@ -0,0 +1,36 @@
+import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
+import WebHookVerb from "App/Models/System/WebHookVerb";
+
+export default class WebHookVerbsController {
+  public async index({ auth, request, response }: HttpContextContract) {
+    var {
+      associations = [],
+      page = 1,
+      perPage = 100,
+      sortBy = "name",
+      sortOrder = "asc",
+    } = request.get();
+
+    var query = WebHookVerb.query().orderBy(sortBy, sortOrder);
+
+    for (const association of associations) query.preload(association);
+
+    query.apply((scopes) => scopes.byUser(auth.user));
+
+    const results = await query.paginate(page, perPage);
+
+    return response.json(results);
+  }
+
+  public async create({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async edit({}: HttpContextContract) {}
+
+  public async update({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
